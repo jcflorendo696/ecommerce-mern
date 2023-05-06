08b29e673d26e3524d938e7f523e387d265b9129
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-07 07:18:26 +0800

    Updated routes. Getting product data from db via Models and Mongoose.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import products from './data/products.js';
-
+import productRoutes from './routes/productRoutes.js';
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -11,18 +13,6 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-
-app.get('/api/products', (req,res) => {
-    res.json(products);
-});
-
-
-
-
-
-app.get('/api/products/:id', (req,res)=>{
-    const product = products.find( (item) => item._id === req.params.id );
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
