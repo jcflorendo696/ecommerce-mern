939c8d1c71ba7732453319e4f54c04c05da3f63d
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-08 03:10:27 +0800

    Added error message when invalid request is submitted to REST endpoint.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -14,5 +15,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
