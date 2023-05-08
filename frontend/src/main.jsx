4bf24ffe2aac9de3d0156bc8472ad441b3e0d30f
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-08 18:22:09 +0800

    Added RTK boilerplate.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,8 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import HomeScreen from './screens/Homescreen';
 import ProductScreen from './screens/ProductScreen';
 
+import { Provider } from 'react-redux';
+import store from './store.js';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +23,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   </React.StrictMode>,
 )
