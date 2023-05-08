a72a14f71c797fc97156e8eb6a5e9084987b946b
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-08 21:26:50 +0800

    Replaced axios with RTK Query on ProductDetails page.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,70 +1,66 @@
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState({});
-    const { id:productId } = useParams();
-
-    useEffect(()=>{
-        const fetchProducts = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-        fetchProducts();
-    },[productId]);
+    const { id: productId } = useParams();
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
     return (
         <>
             <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+            { isLoading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <div>{ error?.data?.message || error.error }</div>
+            ) : (
+                            <Row>
+                            <Col md={5}>
+                                <Image src={ product.image } alt={ product.name } fluid></Image>
+                            </Col>
+                            <Col md={4}>
+                                <ListGroup>
+                                    <ListGroup.Item>
+                                        <h3>{ product.name }</h3>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Rating value={ product.rating } text={`${ product.numReviews } reviews`}></Rating>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <p>{product.description}</p>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Col>
+                            <Col md={3}>
+                                <Card>
+                                    <ListGroup>
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>Price: </Col>
+                                                <Col>
+                                                    <strong>${product.price}</strong>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>Status: </Col>
+                                                <Col>
+                                                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                        <ListGroup.Item>
+                                            <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add to Cart</Button>
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                </Card>
+                            </Col>
+                        </Row>
+            ) }
 
-            <Row>
-                <Col md={5}>
-                    <Image src={ product.image } alt={ product.name } fluid></Image>
-                </Col>
-                <Col md={4}>
-                    <ListGroup>
-                        <ListGroup.Item>
-                            <h3>{ product.name }</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value={ product.rating } text={`${ product.numReviews } reviews`}></Rating>
-                        </ListGroup.Item>
-                        <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-                        <ListGroup.Item>
-                            <p>{product.description}</p>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Price: </Col>
-                                    <Col>
-                                        <strong>${product.price}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Status: </Col>
-                                    <Col>
-                                        <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add to Cart</Button>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
         </>
     )
 }
