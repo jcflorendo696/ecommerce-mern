c09d5f09605ac6bb83e2dcc84932cb20a6b2e786
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-10 02:45:48 +0800

    Cart Badge Feature.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const { cartItems } = useSelector((state)=> state.cart );
+
     return(
         <header>
             <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -16,7 +19,11 @@ const Header = () => {
                         <Nav className="ms-auto">
 
                             <LinkContainer to="/cart">
-                                <Nav.Link><FaShoppingCart/> Cart</Nav.Link>
+                                <Nav.Link><FaShoppingCart/> Cart { cartItems.length > 0 && 
+                                        <Badge pill bg='success' style={{marginLeft: '5px'}}>
+                                                    { cartItems.reduce((acc, item)=> acc + item.qty, 0) }
+                                        </Badge>
+                                }</Nav.Link>
                             </LinkContainer>
 
                             <LinkContainer to="/login">
