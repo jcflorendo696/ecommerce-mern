566a87736b7a160e86f3229d529221d127499616
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-11 02:50:10 +0800

    Added auth endpoints and middlewares(protect, admin)

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -14,6 +15,9 @@ const app = express();
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('API is running...');
