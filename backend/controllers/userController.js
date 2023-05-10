414bd69a9d741dba475b9e9c9c31d39007c7c56f
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-10 18:08:19 +0800

    Added email and pass authentication. Login.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,24 @@
 import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/userModel.js';
+import User from '../models/userModel.js';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.send('auth user');
+    const { email, password } = req.body;
+    const user = await User.findOne({ email })
+    
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 // @desc    Register user
