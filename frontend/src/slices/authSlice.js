c6065bc812c5fb18849dd0479361d27a47421e92
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-12 03:47:46 +0800

    Added Logout functionality.

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -12,10 +12,15 @@ const authSlice = createSlice({
         setCredentials: (state, action) => {
             state.userInfo = action.payload,
             localStorage.setItem('userInfo', JSON.stringify(action.payload));
+        },
+        logout: (state, action) => {
+            // Delete userInfo details in localStorage
+            state.userInfo = null,
+            localStorage.removeItem('userInfo');
         }
     },
 });
 
-export const { setCredentials } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 
 export default authSlice.reducer;
