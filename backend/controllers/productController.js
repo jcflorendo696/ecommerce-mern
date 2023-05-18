611090973d084ab0e83949e77ee5a84519e6b7db
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-18 20:57:23 +0800

    Added edit product functionality.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,25 @@ const getProductById = asyncHandler(async (req, res) => {
 });
 
 
-export { getProducts, getProductById }
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Sample Name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample Category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description',
+    });
+
+    const createdProduct = await product.save();
+    res.send(200).json(createProduct);
+});
+
+
+export { getProducts, getProductById , createProduct };
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -3,15 +3,28 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaTimes, FaCheck, FaEdit, FaTrash } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loading';
-import { useGetProductsQuery } from '../../slices/productsApiSlice';
+import { useGetProductsQuery, useCreateProductMutation } from '../../slices/productsApiSlice';
+import { toast } from 'react-toastify';
 
 const ProductListScreen = () => {
 
-    const { data:products, isLoading, error } = useGetProductsQuery();
+    const { data:products, isLoading, error, refetch } = useGetProductsQuery();
 
+    const [ createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
 
     const deleteHandler = (id) => {
 
+    }
+
+    const createProductHandler = async () => {
+        if(window.confirm('Are you sure you want to create a new product?')){
+            try{
+                await createProduct();
+                refetch();
+            }catch(err){
+                toast.error(err?.data?.message || err.error);
+            }
+        }
     }
 
     return (
@@ -21,11 +34,13 @@ const ProductListScreen = () => {
                 <h1>Products</h1>
             </Col>
             <Col className='text-end'>
-                <Button className='btn-sm m-3'>
+                <Button className='btn-sm m-3' onClick={ createProductHandler }>
                     <FaEdit></FaEdit> Create Product
                 </Button>
             </Col>
         </Row>
+
+        { loadingCreate && <Loader></Loader> }
 
         { isLoading ? (
             <Loader></Loader>
