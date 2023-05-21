2f69c2c4021f9d533a1bae2e0998c3d0288ceea0
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-22 04:16:28 +0800

    Completed Pagination on products and admin productList page.

--- frontend/src/screens/Homescreen.jsx
+++ frontend/src/screens/Homescreen.jsx
@@ -4,6 +4,7 @@ import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loading';
 import Message from '../components/Message';
 import { useParams } from 'react-router-dom';
+import Paginate from '../components/Paginate';
 
 
 const HomeScreen = () => {
@@ -28,6 +29,7 @@ const HomeScreen = () => {
                         </Col>
                     ) )}
                 </Row>
+                <Paginate pages={data.pages} page={data.page}/>
                 </>
             ) }
         </>
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -5,10 +5,13 @@ import Message from '../../components/Message';
 import Loader from '../../components/Loading';
 import { useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation } from '../../slices/productsApiSlice';
 import { toast } from 'react-toastify';
+import { useParams } from 'react-router-dom';
+import Paginate from '../../components/Paginate';
 
 const ProductListScreen = () => {
+    const { pageNumber } = useParams();
 
-    const { data:products, isLoading, error, refetch } = useGetProductsQuery();
+    const { data, isLoading, error, refetch } = useGetProductsQuery({pageNumber});
 
     const [ createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
 
@@ -71,7 +74,7 @@ const ProductListScreen = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {products.map((product)=>(
+                        {data.products.map((product)=>(
                             <tr key={product._id}>
                                 <td>{product._id}</td>
                                 <td>{product.name}</td>
@@ -88,6 +91,7 @@ const ProductListScreen = () => {
                         ))}
                     </tbody>
                 </Table>
+                <Paginate pages={data.pages} page={data.page} isAdmin={true}/>
             </>
         )}
         </>
