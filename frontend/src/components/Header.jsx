f47c5a469c8c605ae927a5595aae718ff11563ce
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-22 06:52:42 +0800

    Added search component.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 import { useNavigate } from 'react-router-dom';
+import SearchBox from './SearchBox';
 
 const Header = () => {
     const { cartItems } = useSelector((state)=> state.cart );
@@ -36,7 +37,7 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-
+                            <SearchBox></SearchBox>
                             <LinkContainer to="/cart">
                                 <Nav.Link><FaShoppingCart/> Cart { cartItems.length > 0 && 
                                         <Badge pill bg='success' style={{marginLeft: '5px'}}>
--- frontend/src/screens/Homescreen.jsx
+++ frontend/src/screens/Homescreen.jsx
@@ -5,7 +5,7 @@ import Loader from '../components/Loading';
 import Message from '../components/Message';
 import { useParams } from 'react-router-dom';
 import Paginate from '../components/Paginate';
-
+import { Link } from 'react-router-dom';
 
 const HomeScreen = () => {
     const { pageNumber, keyword } = useParams();
@@ -14,6 +14,7 @@ const HomeScreen = () => {
 
     return (
         <>
+            { keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link>}
             { isLoading ? (
                 <Loader></Loader>
             ) : error ? (
