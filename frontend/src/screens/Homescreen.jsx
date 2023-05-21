3cf7eb919512e22a0b9c808fa95dda595b4c6f9c
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-22 06:24:04 +0800

    Added searchProducts routes.

--- frontend/src/screens/Homescreen.jsx
+++ frontend/src/screens/Homescreen.jsx
@@ -8,9 +8,9 @@ import Paginate from '../components/Paginate';
 
 
 const HomeScreen = () => {
-    const { pageNumber } = useParams();
+    const { pageNumber, keyword } = useParams();
 
-    const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+    const { data, isLoading, error } = useGetProductsQuery({ pageNumber, keyword });
 
     return (
         <>
@@ -29,7 +29,7 @@ const HomeScreen = () => {
                         </Col>
                     ) )}
                 </Row>
-                <Paginate pages={data.pages} page={data.page}/>
+                <Paginate pages={data.pages} page={data.page} keyword={keyword ? keyword : ''} />
                 </>
             ) }
         </>
