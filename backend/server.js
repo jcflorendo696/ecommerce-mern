9aa0d8dc249805572c322d9e2abfabc02889d69a
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-05-29 19:35:32 +0800

    Adjustments for deployment.

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -37,6 +34,18 @@ app.get('/api/config/paypal', (req, res) => res.send({
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    // any route that is not API will be redirected to index.html
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+}else{
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
