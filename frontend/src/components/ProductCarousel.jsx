a4e9385c2aca79d394f33d5fd03fce041fd7cbd4
John Florendo <jeyz@Johns-MacBook-Pro.local>
2023-06-01 15:02:44 +0800

    Static data for now in carousel homepage because of Render platform not persisting data on free tier.

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -15,7 +15,7 @@ const ProductCarousel = () => {
         <Message variant='danger'>{error}</Message>
     ) : (
         <Carousel pause='hover' className='bg-primary mb-4'>
-            {products.map( product => (
+            {/* {products.map( product => (
                 <Carousel.Item key={product._id}>
                     <Link to={`/product/${product._id}`}>
                         <Image src={product.bannerImage} alt={product.name} fluid></Image>
@@ -24,7 +24,33 @@ const ProductCarousel = () => {
                         </Carousel.Caption>
                     </Link>
                 </Carousel.Item>
-            ))}
+            ))} */}
+                <Carousel.Item key='1'>
+                    <Link to={`/product/64551569330a544c48354829`}>
+                        <Image src='/images/iphone-14-banner-2.jpg' alt='iPhone 14 Pro' fluid></Image>
+                        <Carousel.Caption className='carousel-caption'>
+                            <h2>iPhone 14 Pro ($599.99)</h2>
+                        </Carousel.Caption>
+                    </Link>
+                </Carousel.Item>
+                <Carousel.Item key='2'>
+                    <Link to={`/product/64551569330a544c4835482b`}>
+                        <Image src='/images/ps5-banner-2.jpg' alt='Sony Playstation 5 Pro White Version' fluid></Image>
+                        <Carousel.Caption className='carousel-caption'>
+                        <h2>Sony Playstation 5 Pro White Version ($399.99)</h2>
+                        </Carousel.Caption>
+                    </Link>
+                </Carousel.Item>
+                <Carousel.Item key='3'>
+                    <Link to={`/product/64551569330a544c48354828`}>
+                        <Image src='/images/airpods-pro-banner-2.jpg' alt='Airpods Wireless Bluetooth Headphones' fluid></Image>
+                        <Carousel.Caption className='carousel-caption'>
+                        <h2>Airpods Wireless Bluetooth Headphones ($89.99)</h2>
+                        </Carousel.Caption>
+                    </Link>
+                </Carousel.Item>
+
+
         </Carousel>
     )
 
